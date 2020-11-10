06aaa7aa218b48581589fb569d1ca4fb727f20be
Sky <skyvorobey@icloud.com>
2020-11-10 19:17:19 +0200

    убрал разметку index.php

--- dz6/index.php
+++ dz6/index.php
@@ -1,22 +1,5 @@
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Домашняя задание №6</title>
-</head>
-<header>
-    <h1>Домашняя задание №6.</h1>
-</header>
-<body>
-    <?php include __DIR__ . '/classes/GuestBook.php';
+<?php include __DIR__ . '/classes/GuestBook.php';
 
-    $guestbook = new GuestBook('data');
-    var_dump($guestbook);
-    ?>
-
-
-</body>
-</html>
+$guestbook = new GuestBook('data');
+var_dump($guestbook);
+?>
