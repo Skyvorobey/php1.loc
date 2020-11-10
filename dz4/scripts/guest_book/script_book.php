7776acf24dca3fcc5c0c9f014d0cc601441235fe
Sky <skyvorobey@icloud.com>
2020-11-10 20:05:01 +0200

    Поправил скрипт гостевой книги, добавил дату и время перед сообщением

--- dz4/scripts/guest_book/script_book.php
+++ dz4/scripts/guest_book/script_book.php
@@ -3,7 +3,10 @@ if (isset ($_POST['submit'])) {
     if (($_POST['new_rec']) !== '') {
         $file = fopen(__DIR__ . '/../../data/guest_book.txt', 'a');
         $new_r = ($_POST['new_rec']);
-        fwrite($file, '<br>') . fwrite($file, $new_r);
+        fwrite($file, ' ') .
+        fwrite($file, date('d.m.Y H:i:s', time())) .
+        fwrite($file, ' ') .
+        fwrite($file, $new_r);
         fclose($file);
 
         $host  = $_SERVER['HTTP_HOST'];
