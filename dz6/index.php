a39098eddc6e22e1091e72b8c37ef89e4b2e4d0c
Sky <skyvorobey@icloud.com>
2020-11-11 18:11:18 +0200

    в индексе прописал функцию вывода комментов

--- dz6/index.php
+++ dz6/index.php
@@ -1,4 +1,40 @@
 <?php include __DIR__ . '/classes/GuestBook.php';
 $guestbook = new GuestBook(__DIR__ . '/data/guest_book.txt');
+$guestbook->append('Тест');
 var_dump($guestbook);
+
+?>
+<div style="border: 1px solid #ccc; padding: 15px;">
+    <b>Иванов Иван</b> (11.11.2020 16:25)
+    <div style="font-style: italic;">
+        Привет!
+    </div>
+</div>
+
+<?php
+
+$path = __DIR__ . '/data/guest_book.txt';
+
+function echo_guest_book($path) {
+    if (file_exists($path) === true) {
+        $res = fopen($path, 'r');
+
+        echo '<div style="border: 1px solid #ccc; padding: 15px;">';
+        while (!feof($res)) {
+            $array = fgets($res);
+            $explode_array = explode(',', $array);
+            $name = $explode_array['0'];
+            $when = $explode_array['1'];
+            $comment = $explode_array['2'];
+            echo '<b>' . $name . '</b>' . $when;
+            echo '<div>' . $comment . '</div>';
+        }
+        echo '</div>';
+    }   else {
+        echo 'Не верный путь:' . $path;
+    }
+}
+echo echo_guest_book($path);
+
+
 ?>
