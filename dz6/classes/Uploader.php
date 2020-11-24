aa1355983962d8db256e78fe5603b1fb19ab5582
Sky <skyvorobey@icloud.com>
2020-11-24 18:33:47 +0200

    залил первый вариант загрузчика

--- dz6/classes/Uploader.php
+++ dz6/classes/Uploader.php
@@ -1,22 +1,32 @@
 <?php
-include __DIR__ . '/TextFile.php';
-class Uploader extends TextFile
+
+class Uploader
 {
-    public function __construct($field_name_form)
+    public $file_img;
+    public function __construct($file_img)
     {
-        $this->data = $field_name_form;
+        $this->file_img = $file_img;
     }
 
     public function isUploaded() {
-        if ($_FILES[$this->data] === 1) {
+
+        if (isset(($_FILES[$this->file_img]))) {
+            if ((0 === $_FILES[$this->file_img]['error']) &&
+            (('image/png' === $_FILES[$this->file_img]['type']) ||
+                'image/jpeg' === $_FILES[$this->file_img]['type']))  {
             return true;
+        }   else
+            echo 'Досупные форматы для загрузки "png" и "jpeg" ';
+            return false;
         }
-        return false;
     }
 
-   public function upload() {
-        if ($this->isUploaded()) {
-            move_uploaded_file($_FILES[$this->data]['tmp_name'], __DIR__ . '/images/' . $_FILES[$this->data]['name']);
+    public function upload() {
+        if ($this->isUploaded() === true) {
+           if (isset($_FILES[$this->file_img])) {
+               move_uploaded_file($_FILES[$this->file_img]['tmp_name'],
+                   __DIR__ . '/images/' . $_FILES[$this->file_img]['name'] );
+            }
         }
     }
 }
