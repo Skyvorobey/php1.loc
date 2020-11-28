7877343a6d00980ed8f6bcec6ee030f2486877c4
Sky <skyvorobey@icloud.com>
2020-11-28 13:55:22 +0200

    отладка объекта

--- dz7/index.php
+++ dz7/index.php
@@ -1,1 +1,12 @@
 <?php
+    require_once 'application/views/view.php';
+
+    $view = new View;
+    $view->assign('vasya', 1);
+    $view->assign('petya', 2);
+
+    //отладка
+    var_dump($view);
+    var_dump($view->transmitted_value);
+    
+    $view->display('test_template');
