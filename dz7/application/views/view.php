bfc8ebe038c99b397f3dcb315c8dd82977f640d3
Sky <skyvorobey@icloud.com>
2020-11-28 13:56:28 +0200

    создал класс 'View'

--- dz7/application/views/view.php
+++ dz7/application/views/view.php
@@ -1,7 +1,20 @@
 <?php
 
-
-class view
+class View
 {
+    public $transmitted_value; //public сменить на protected после отладки
+    public $return_template; //public сменить на protected после отладки
 
+            //assign = 'назначить';
+    public function assign($name, $value) {
+        return $this->transmitted_value[] = $name . ',' . $value . "\n";
+    }
+
+    public function display($template) {
+        require_once __DIR__ . '/templates' . "/$template" . '.php';
+    }
+
+    public function render($template) {
+        return $this->return_template = __DIR__ . '/templates' . "/$template" . '.php';
+    }
 }
